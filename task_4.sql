94fbbeb1e47df5afabef91f7258470681791aaf1
maxq1seg <maximb00131516@gmail.com>
2021-11-17 15:05:41 +0300

    zhenya pidor 2

--- task_4.sql
+++ task_4.sql
@@ -1,17 +1,23 @@
 select 
-    dep_id,
-    department_name,
-    to_date('2021-11-17', 'yyyy-MM-dd')- stats.youngest as youngest_in_days,
-    to_date('2021-11-17', 'yyyy-MM-dd') - stats.oldest as oldest_in_days
+        subquery.dep_id,
+        d2.DEPARTMENT_NAME as dep_name,
+        e2.first_name || ' ' || e2.last_name AS oldest_emp_name,
+        to_date('2021-11-17', 'yyyy-MM-dd')-subquery.oldest as oldest,
+        to_date('2021-11-17', 'yyyy-MM-dd')-subquery.youngest as youngest
 from (
-SELECT
-    d.department_id as dep_id,
-    max(hire_date) as youngest,
-    min(hire_date) as oldest
-FROM employees e
-		JOIN departments d 
-        on e.department_id=d.department_id
-GROUP BY d.department_id
-) stats
-join departments d2
-on dep_id=d2.DEPARTMENT_ID
+    SELECT
+            d.department_id as dep_id,
+            min(hire_date) as oldest,
+            max(hire_date) as youngest
+        FROM employees e
+                JOIN departments d 
+                on e.department_id=d.department_id
+    GROUP BY d.department_id
+) subquery
+right join EMPLOYEES e2
+on e2.DEPARTMENT_ID=subquery.dep_id
+
+left join departments d2
+on d2.DEPARTMENT_ID=subquery.dep_id
+
+where e2.HIRE_DATE=subquery.oldest;
